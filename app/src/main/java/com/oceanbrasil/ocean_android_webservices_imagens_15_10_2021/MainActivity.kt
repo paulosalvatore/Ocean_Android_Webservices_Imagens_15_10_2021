646479075d0e9931d7796d99407c3a228658ffc9
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-15 12:06:08 -0300

    Demonstração de implementação do Glide para carregamento de imagens

--- app/src/main/java/com/oceanbrasil/ocean_android_webservices_imagens_15_10_2021/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_webservices_imagens_15_10_2021/MainActivity.kt
@@ -2,8 +2,10 @@ package com.oceanbrasil.ocean_android_webservices_imagens_15_10_2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -54,5 +56,12 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this@MainActivity, "Falha na requisição", Toast.LENGTH_SHORT).show()
             }
         })
+
+        // Carregamento de imagem (trataremos mais sobre isso na aula de RecyclerView)
+
+        val ivPokemon = findViewById<ImageView>(R.id.ivPokemon)
+
+        val imagemBulbasaur = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png"
+        Glide.with(this).load(imagemBulbasaur).into(ivPokemon)
     }
 }
